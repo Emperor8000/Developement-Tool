0a8004867620e681c26645c970ce01103c260c22
Sean <sps20009@utdallas.edu>
2023-04-30 20:50:55 -0500

    Attempt to load guns automatically
    basically said it all in the title, but I think I will remove this feature in favor of fixing weapon spawning and perhaps adding a reload feature but I'm not sure about the latter.

--- Assets/_Game/Scripts/sc_GunSelector.cs
+++ Assets/_Game/Scripts/sc_GunSelector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [DisallowMultipleComponent]
 [ExecuteInEditMode]
@@ -9,6 +10,7 @@ public class sc_GunSelector : MonoBehaviour
     [SerializeField] private string ActiveGunName;
     [SerializeField] public Transform GunParent;
     [SerializeField] public List<sc_GunConfiguration> Guns;
+    public sc_GunConfiguration[] allGuns;
 
     //[Space]
     //[Header("Runtime Filled")]
@@ -16,6 +18,45 @@ public class sc_GunSelector : MonoBehaviour
 
     private void Start()
     {
+        string fullPath = $"{Application.dataPath}/{Guns}";
+        if (!System.IO.Directory.Exists(fullPath))
+        {
+            return;
+        }
+
+        var folders = new string[] { $"Assets/_Game/Scriptable Objects/{Guns}" };
+        var guides = AssetDatabase.FindAssets("t:sc_GunConfiguration", folders);
+
+        var GunsList = new sc_GunConfiguration[guides.Length];
+
+        bool mismatch;
+        if (allGuns == null)
+        {
+            mismatch = true;
+            allGuns = GunsList;
+        }
+        else
+        {
+            mismatch = GunsList.Length != GunsList.Length;
+        }
+
+        for (int i = 0; i < GunsList.Length; i++)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guides[i]);
+            GunsList[i] = AssetDatabase.LoadAssetAtPath<sc_GunConfiguration>(path);
+            mismatch |= (i < allGuns.Length && allGuns[i] != GunsList[i]);
+        }
+
+        if (mismatch)
+        {
+            allGuns = GunsList;
+        }
+
+        for(int i = 0; i < allGuns.Length; i++)
+        {
+            Guns.Add(allGuns[i]);
+        }
+
         sc_GunConfiguration gun = Guns.Find(gun => gun.Name == ActiveGunName);
 
         if (gun == null)
@@ -33,6 +74,6 @@ public class sc_GunSelector : MonoBehaviour
 
     public void SwitchWeapon(sc_GunConfiguration newActiveGun)
     {
-        
+        //I think I don't have time for this
     }
 }
